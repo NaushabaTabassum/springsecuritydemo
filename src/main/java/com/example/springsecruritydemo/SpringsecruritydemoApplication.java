0af63ee64da3ae724adbc7320d1d5a26931046b1
Naushaba Tabassum <tnaushaba@yahoo.com>
2021-07-14 11:32:15 -0500

    Spring security

---
+++ src/main/java/com/example/springsecruritydemo/SpringsecruritydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecruritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecruritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecruritydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecruritydemo/SpringsecruritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecruritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecruritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
